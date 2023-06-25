9e4bb71e5fdf4c2cac30ad9a03dea1c19fe26872
Nigel D. Watkins <nigeldwatkins@gmail.com>
2023-06-25 10:49:39 -0400

    index.js

--- index.js
+++ index.js
@@ -37,4 +37,31 @@ function start() {
             "Exit",
         ],
     })
-};
+    //
+    .then((answer) => {
+        switch (answer.action) {
+            case "View all departments":
+                viewAllDepartments();
+                break;
+            case "View all roles":
+                viewAllRoles();
+                break;
+            case "View employees":
+                viewAllEmployees();
+                break;
+            case "Add a department":
+                addDepartment();
+                break;
+            case "Add an employee":
+                addEmployee();
+                break;
+            case "Update an employee role":
+                updateEmployeeRole();
+                break;
+            case "Exit":
+                db.end();
+                console.log("Bye");
+                break;
+            }
+    });
+}
